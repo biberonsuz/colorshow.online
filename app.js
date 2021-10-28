03c340d821485b5e2b0ddf41b11165b3291faca5
biberonsuz <pinarkzk@gmail.com>
2021-10-28 22:40:39 +0300

    cleanup file system

--- app.js
+++ app.js
@@ -3,19 +3,13 @@ const express = require("express")
 // create an express app
 const app = express()
 
-
-
 // use the express-static middleware
 app.use(express.static("public"))
 
 // define the first route
-app.get("/", function (req, res) {
-})
-
-
-//passsing directoryPath and callback function
+app.get("/", function (req, res) {})
 
 // start the server listening for requests
 app.listen(process.env.PORT || 80, 
-	() => console.log("Server is running..."))
+	() => console.log("Server is running..."));
 
